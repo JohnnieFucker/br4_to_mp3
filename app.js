b558dfc3302736cdccf0cc765ca9e401accf87ae
JohnnieFucker <shine@vip.qq.com>
2015-12-28 18:26:21 +0800

    init repo

---
+++ app.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+var path = require('path');
+
+var baseUrl = process.cwd();
+fs.readdir('./br4_files', function (err, files) {
+    if (err) {
+        throw err;
+    }
+    files.forEach(function (file) {
+        var fileName = path.basename(file, '.BR4')+'.mp3';
+        var readStream = fs.createReadStream('./br4_files/'+file);
+        var writeStream = fs.createWriteStream('./mp3_files/'+fileName);
+        readStream.on('data', function(chunk) {
+            var decodeChunk = new Buffer(chunk.length);
+            for(var i=0;i<chunk.length;i++){
+                decodeChunk[i]= ~ chunk[i];
+            }
+            if (writeStream.write(decodeChunk) === false) {
+                readStream.pause();
+            }
+        });
+        readStream.on('end', function() {
+            writeStream.end();
+        });
+        writeStream.on('drain', function() {
+            readStream.resume();
+        });
+        writeStream.on('finish', function() {
+           console.log('decode ' + file+' done!');
+        });
+    });
+});
