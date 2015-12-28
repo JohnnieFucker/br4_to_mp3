e7f9a7e730d29f7bc9289d4f818f68dffeaac409
JohnnieFucker <shine@vip.qq.com>
2015-12-28 18:55:12 +0800

    init repo

--- app.js
+++ app.js
@@ -7,26 +7,29 @@ fs.readdir('./br4_files', function (err, files) {
         throw err;
     }
     files.forEach(function (file) {
-        var fileName = path.basename(file, '.BR4')+'.mp3';
-        var readStream = fs.createReadStream('./br4_files/'+file);
-        var writeStream = fs.createWriteStream('./mp3_files/'+fileName);
-        readStream.on('data', function(chunk) {
-            var decodeChunk = new Buffer(chunk.length);
-            for(var i=0;i<chunk.length;i++){
-                decodeChunk[i]= ~ chunk[i];
-            }
-            if (writeStream.write(decodeChunk) === false) {
-                readStream.pause();
-            }
-        });
-        readStream.on('end', function() {
-            writeStream.end();
-        });
-        writeStream.on('drain', function() {
-            readStream.resume();
-        });
-        writeStream.on('finish', function() {
-           console.log('decode ' + file+' done!');
-        });
+        var ext = path.extname(file);
+        if(ext.toUpperCase()=='.BR4'){
+            var fileName = path.basename(file, '.BR4')+'.mp3';
+            var readStream = fs.createReadStream('./br4_files/'+file);
+            var writeStream = fs.createWriteStream('./mp3_files/'+fileName);
+            readStream.on('data', function(chunk) {
+                var decodeChunk = new Buffer(chunk.length);
+                for(var i=0;i<chunk.length;i++){
+                    decodeChunk[i]= ~ chunk[i];
+                }
+                if (writeStream.write(decodeChunk) === false) {
+                    readStream.pause();
+                }
+            });
+            readStream.on('end', function() {
+                writeStream.end();
+            });
+            writeStream.on('drain', function() {
+                readStream.resume();
+            });
+            writeStream.on('finish', function() {
+                console.log('decode ' + file+' done!');
+            });
+        }
     });
 });
